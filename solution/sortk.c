5f4678f25c14d999bec2d85d85ee66d7fc8fca94
Noxta-code <colin.m.kr@gmail.com>
2025-01-30 17:58:56 -0600

    Updated sortk.c code to make it a lot cleaner and hopefully get close to bug fixing

--- solution/sortk.c
+++ solution/sortk.c
@@ -1,41 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-// Comparison function to use for qsort()
-// Each parameter is a pointer to one of the elements being compared
-int compare_kword(const void *one, const void *two){
-    // Parameter types are void pointers for qsort and const so they aren't changed
-    // Casting one and two to char** interprets them as pointers to arrays of char*
-    // Dereferences lines[i][1] to compare the strings at the kth word pointer
-
-    char **line_one = (char**)one; // Pointer to the first spot of 1st line
-    char **line_two = (char**)two; // Pointer to the first spot of 2nd line
-    // Each line is an array of two char* pointers ([0] points to the line start, [1] points to kth word)
-
-    /*
-    // Print the lines and the kth words for debugging
-    printf("Comparing:\n%s\n%s\n", line_one[0], line_two[0]);
-    if (line_one[1]) printf("Kth word 1: %s\n", line_one[1]);
-    if (line_two[1]) printf("Kth word 2: %s\n", line_two[1]);
-    printf("%ld\n", strlen(line_one[0]));
-    printf("%ld\n", strlen(line_two[0]));
-    printf("%ld\n", strlen(line_one[0]) - strlen(line_two[0]));
-    */
-
-
+int compare_kword(const void *one, const void *two) {
+    char **line_one = (char **)one;
+    char **line_two = (char **)two;
 
     // Check for lines that have fewer than k words (key word is NULL)
-    // Both lines have <k words, so they're comparable using the whole line
-    if(line_one[1] == NULL && line_two[1] == NULL){
+    if (line_one[1] == NULL && line_two[1] == NULL) {
         return strcmp(line_one[0], line_two[0]);
-    }
-    // Only the first line has <k words, not comparable, ignore
-    else if(line_one[1] == NULL){
+    } else if (line_one[1] == NULL) {
         return 1; // place one after two
-    }
-    else if(line_two[1] == NULL){
+    } else if (line_two[1] == NULL) {
         return -1; // place two after one
     }
 
@@ -43,70 +19,50 @@ int compare_kword(const void *one, const void *two){
     line_one[1][strcspn(line_one[1], "\n")] = '\0';
     line_two[1][strcspn(line_two[1], "\n")] = '\0';
 
-    // Else, compare the keywords
-    // Handle trailing newline character for edge case, find which kword is shorter b/c that one has no newline
-    int result = -1;
-    if(strlen(line_two[1]) > strlen(line_one[1])){
-        result = strncmp(line_one[1], line_two[1], strlen(line_one[1]));
-    }
-    else{
-        result = strncmp(line_one[1], line_two[1], strlen(line_two[1]));
-    }
-    /*
-    printf("First keyword is %s \n", line_one[1]);
-    printf("Second keyword is %s \n", line_two[1]);
-    printf("Keyword result is %d \n", result);
-    */
+    // Compare the kth words
+    int result = strcmp(line_one[1], line_two[1]);
 
     // If kth words were different, return the result
-    if(result != 0){
+    if (result != 0) {
         return result;
     }
 
-    // If kth words were the same, compare whole string
+    // If kth words were the same, compare the whole line
     return strcmp(line_one[0], line_two[0]);
-
-    // If kth words were the same, return the shorter string
-    //return strlen(line_one[0]) - strlen(line_two[0]);
 }
 
 int main(int argc, char *argv[]) {
-
     // Check for correct number of arguments
-    if(argc != 3){
-        //printf("Incorrect number of inputs. \n");
+    if (argc != 3) {
         return 1;
     }
 
     // Check that the second argument is an integer
     char *invalidPtr;
     int order = strtol(argv[2], &invalidPtr, 10);
-    if(*invalidPtr != '\0'){ // Meaning there are non-int chars
-        //printf("k must be an integer.\n");
+    if (*invalidPtr != '\0') { // Meaning there are non-int chars
         return 1;
     }
 
     // Check if k <= 0
-    if(order <= 0){
-        //printf("k less than or equal to 0. \n");
+    if (order <= 0) {
         return 1;
     }
 
     // Open the file
     FILE *input = fopen(argv[1], "r"); // Open in read mode
-    if (input == NULL){
-        //printf("Error opening file");
+    if (input == NULL) {
         return 1;
     }
 
     // Create variables for reading input
     char *line = NULL; // Pointer to the line
     size_t length = 0; // Size of the buffer for curr line
-    ssize_t read; // If -1 then end' of the file has been reached
+    ssize_t read; // If -1 then end of the file has been reached
     int lineNum = 0; // How many lines input.txt has
 
     // Count how many lines the input file has
-    while ((read = getline(&line, &length, input)) != -1){
+    while ((read = getline(&line, &length, input)) != -1) {
         lineNum++;
     }
 
@@ -118,7 +74,7 @@ int main(int argc, char *argv[]) {
 
     // Get the kth word of each line
     int i = 0;
-    while((read = getline(&line, &length, input)) != -1){
+    while ((read = getline(&line, &length, input)) != -1) {
         lines[i][0] = strdup(line); // Allocate memory for the line
 
         // Split the line into words (tokens)
@@ -126,9 +82,9 @@ int main(int argc, char *argv[]) {
         int word_count = 0;
 
         // Find the kth word for this line
-        while(word != NULL){
+        while (word != NULL) {
             word_count++;
-            if(word_count == order){
+            if (word_count == order) {
                 lines[i][1] = strdup(word); // Add a copy of the kth word to the array
                 break;
             }
@@ -136,41 +92,36 @@ int main(int argc, char *argv[]) {
         }
 
         // If the line is too short, set the kth word to NULL
-        if(word_count < order){
+        if (word_count < order) {
             lines[i][1] = NULL;
         }
 
         i++;
     }
 
-    // Sort the lines by kth word. Can use sizeof because it's the pointer, not the actual line
+    // Sort the lines by kth word
     qsort(lines, lineNum, sizeof(lines[0]), compare_kword);
 
     // Print the sorted lines
-    for(int i=0; i<lineNum; i++){
-        printf("%s", lines[i][0]); // %s to print a string from a pointer to the first char
-
-        // Check if the line ends with a newline character
-        // lines[i][0] holds a pointer to the string, so access the last element with strlen(lines[i][i]-1)
-    if (lines[i][0][strlen(lines[i][0]) - 1] != '\n') {
-        printf("\n"); // Add a newline if it doesn't exist
+    for (int i = 0; i < lineNum; i++) {
+        printf("%s", lines[i][0]); // Print the line
+        if (lines[i][0][strlen(lines[i][0]) - 1] != '\n') {
+            printf("\n"); // Add a newline if it doesn't exist
+        }
     }
-    }
-
 
     // Free memory
-    for(int i=lineNum-1; i>=0; i--){
-        if(lines[i][1] != NULL){
-            free(lines[i][1]); // Free the full line copy
+    for (int i = lineNum - 1; i >= 0; i--) {
+        if (lines[i][1] != NULL) {
+            free(lines[i][1]); // Free the kth word copy
         }
-        if(lines[i][0] != NULL){
-            free(lines[i][0]); // Free the kth word copy
+        if (lines[i][0] != NULL) {
+            free(lines[i][0]); // Free the full line copy
         }
-        //free(lines[i]); // Free the whole row
     }
 
     fclose(input);
     free(line);
 
-	return 0;
+    return 0;
 }
